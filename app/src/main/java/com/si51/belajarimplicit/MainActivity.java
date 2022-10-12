dbf4720b8bc759d1df090ebd862531bb2452221e
ahmfarisi <ahmfarisi@gmail.com>
2022-10-12 11:18:30 +0700

    initial setting

--- app/src/main/java/com/si51/belajarimplicit/MainActivity.java
+++ app/src/main/java/com/si51/belajarimplicit/MainActivity.java
@@ -3,12 +3,47 @@ package com.si51.belajarimplicit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUrl, etLokasi, etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUrl = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_teks);
+
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
